5c43884481f959b336fae63a1501ba62ca69d17e
Byron Houwens <zhion360@gmail.com>
2020-04-22 13:03:27 +0200

    Add storage node DB for chain

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+/// Path to chain DB
+pub const DB_PATH: &'static str = "src/db";
+
+/// Path to test net DB
+pub const DB_PATH_TEST: &'static str = "test";
+
+/// Path to live net DB
+pub const DB_PATH_LIVE: &'static str = "live";
