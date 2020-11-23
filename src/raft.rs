9167b320c83af5d2c34609103f51554d6728d1a5
Jean-Philippe DUFRAIGNE <jp@dufraigne.eu>
2020-11-23 15:33:16 +0100

    Handle mutliple RAFT compute peers
    Clean up tests.
    Dispatch messages Raft need to send to Raft peers.
    Test for 1, 2 and 20 (expected production) peers.
    Test sending from 2 different peers: Leader or not (Raft will forward to leader).
    
    Test:
    cargo test
    cargo build

--- src/raft.rs
+++ src/raft.rs
@@ -11,16 +11,21 @@ type CommitedSender = mpsc::Sender<Vec<RaftData>>;
 type CommitedReceiver = mpsc::Receiver<Vec<RaftData>>;
 type MsgSender = mpsc::Sender<Msg>;
 type MsgReceiver = mpsc::Receiver<Msg>;
+type RaftMsgSender = mpsc::Sender<Message>;
+type RaftMsgReceiver = mpsc::Receiver<Message>;
 
 struct RaftConfig {
+    cfg: Config,
     msg_rx: MsgReceiver,
     committed_tx: CommitedSender,
+    msg_out_tx: RaftMsgSender,
     timeout_duration: Duration,
 }
 
 enum Msg {
     Propose { data: RaftData },
     Raft(Message),
+    Close,
 }
 
 impl fmt::Debug for Msg {
@@ -28,21 +33,19 @@ impl fmt::Debug for Msg {
         match *self {
             Self::Propose { ref data, .. } => write!(f, "Propose {:?}", data),
             Self::Raft(ref msg) => write!(f, "Raft {:?}", msg),
+            Self::Close => write!(f, "Close"),
         }
     }
 }
 
 async fn run_raft_loop(raft_config: RaftConfig) {
     let RaftConfig {
+        cfg,
         mut msg_rx,
         mut committed_tx,
+        mut msg_out_tx,
         timeout_duration,
     } = raft_config;
-    let cfg = Config {
-        id: 1,
-        peers: vec![1],
-        ..Default::default()
-    };
     let peers = vec![];
 
     let storage = MemStorage::new();
@@ -61,7 +64,7 @@ async fn run_raft_loop(raft_config: RaftConfig) {
                 timeout_at_time = Instant::now() + timeout_duration;
                 node.tick();
             }
-            Ok(None) => {
+            Ok(Some(Msg::Close)) | Ok(None) => {
                 // Disconnected
                 return;
             }
@@ -74,11 +77,15 @@ async fn run_raft_loop(raft_config: RaftConfig) {
             }
         }
 
-        process_ready(&mut node, &mut committed_tx).await;
+        process_ready(&mut node, &mut committed_tx, &mut msg_out_tx).await;
     }
 }
 
-async fn process_ready(node: &mut RawNode<MemStorage>, committed_tx: &mut CommitedSender) {
+async fn process_ready(
+    node: &mut RawNode<MemStorage>,
+    committed_tx: &mut CommitedSender,
+    msg_out_tx: &mut RaftMsgSender,
+) {
     if !node.has_ready() {
         return;
     }
@@ -87,11 +94,11 @@ async fn process_ready(node: &mut RawNode<MemStorage>, committed_tx: &mut Commit
 
     let is_leader = node.raft.leader_id == node.raft.id;
     if is_leader {
-        send_messages_to_followers(node, &mut ready);
+        send_messages_to_peers(msg_out_tx, &mut ready).await;
         update_ready_mut_store(node, &mut ready);
     } else {
         update_ready_mut_store(node, &mut ready);
-        send_messages_to_leader(node, &mut ready);
+        send_messages_to_peers(msg_out_tx, &mut ready).await;
     }
 
     apply_committed_entries(&mut ready, committed_tx).await;
@@ -99,15 +106,9 @@ async fn process_ready(node: &mut RawNode<MemStorage>, committed_tx: &mut Commit
     node.advance(ready);
 }
 
-fn send_messages_to_followers(_node: &mut RawNode<MemStorage>, ready: &mut Ready) {
-    for _msg in ready.messages.drain(..) {
-        // TODO: Send messages to followers.
-    }
-}
-
-fn send_messages_to_leader(_node: &mut RawNode<MemStorage>, ready: &mut Ready) {
-    for _msg in ready.messages.drain(..) {
-        // TODO: Send the messages to the leader.
+async fn send_messages_to_peers(msg_out_tx: &mut RaftMsgSender, ready: &mut Ready) {
+    for msg in ready.messages.drain(..) {
+        msg_out_tx.send(msg).await.unwrap();
     }
 }
 
@@ -144,35 +145,155 @@ async fn apply_committed_entries(ready: &mut Ready, committed_tx: &mut CommitedS
     }
 }
 
-#[tokio::test(threaded_scheduler)]
-async fn test_send_proposal_is_commited() {
-    let (mut msg_tx, msg_rx) = mpsc::channel(100);
-    let (committed_tx, mut committed_rx) = mpsc::channel(100);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::future::join_all;
+    use std::collections::HashMap;
 
-    let main_handle = tokio::spawn(async move {
-        run_raft_loop(RaftConfig {
+    struct TestNode {
+        pub raft_config: Option<RaftConfig>,
+        pub msg_out_rx: Option<RaftMsgReceiver>,
+        pub msg_tx: MsgSender,
+        pub committed_rx: CommitedReceiver,
+    }
+
+    #[tokio::test(threaded_scheduler)]
+    async fn test_send_proposal_is_commited_1_node() {
+        send_proposal_check_commited(1).await;
+    }
+
+    #[tokio::test(threaded_scheduler)]
+    async fn test_send_proposal_is_commited_2_nodes() {
+        send_proposal_check_commited(2).await;
+    }
+
+    #[tokio::test(threaded_scheduler)]
+    async fn test_send_proposal_is_commited_20_nodes() {
+        send_proposal_check_commited(20).await;
+    }
+
+    async fn send_proposal_check_commited(num_peers: u64) {
+        let mut join_handles = Vec::new();
+        let (peer_indexes, mut test_nodes) = test_configs(num_peers);
+        let msg_txs: Vec<_> = test_nodes.iter().map(|node| node.msg_tx.clone()).collect();
+
+        // Setup RAFT: Raft loops and Raft message dispatching loops.
+        for test_node in &mut test_nodes {
+            let raft_config = test_node.raft_config.take().unwrap();
+            join_handles.push(tokio::spawn(async move {
+                run_raft_loop(raft_config).await;
+            }));
+
+            let msg_out_rx = test_node.msg_out_rx.take().unwrap();
+            let peer_indexes = peer_indexes.clone();
+            let msg_txs = msg_txs.clone();
+            join_handles.push(tokio::spawn(async move {
+                dispatch_messages_loop(msg_out_rx, peer_indexes, msg_txs).await;
+            }));
+        }
+
+        // Send a proposal and wait for it to be commited.
+        {
+            let proposed_data = vec![17];
+            send_proposal(&mut test_nodes[0], proposed_data.clone()).await;
+
+            let commited_data = recv_commited(&mut test_nodes).await;
+            let expected = expected_commited(&test_nodes, &vec![proposed_data]);
+            assert_eq!(commited_data, expected);
+        }
+
+        // Send a proposal to other nodeand wait for it to be commited.
+        if num_peers > 1 {
+            let proposed_data = vec![33];
+            send_proposal(&mut test_nodes[1], proposed_data.clone()).await;
+
+            let commited_data = recv_commited(&mut test_nodes).await;
+            let expected = expected_commited(&test_nodes, &vec![proposed_data]);
+            assert_eq!(commited_data, expected);
+        }
+
+        // Close raft loop so spawned task can complete and wait for completion.
+        for test_node in &mut test_nodes {
+            test_node.msg_tx.send(Msg::Close).await.unwrap();
+        }
+        join_all(join_handles).await;
+    }
+
+    async fn dispatch_messages_loop(
+        mut msg_out_rx: RaftMsgReceiver,
+        peer_indexes: HashMap<u64, usize>,
+        mut msg_txs: Vec<MsgSender>,
+    ) {
+        loop {
+            match msg_out_rx.recv().await {
+                Some(msg) => {
+                    let to_index = peer_indexes[&msg.to];
+                    msg_txs[to_index].send(Msg::Raft(msg)).await.unwrap();
+                }
+                None => {
+                    // Disconnected
+                    return;
+                }
+            }
+        }
+    }
+
+    async fn send_proposal(test_node: &mut TestNode, data: RaftData) {
+        test_node.msg_tx.send(Msg::Propose { data }).await.unwrap();
+    }
+
+    async fn recv_commited(test_nodes: &mut Vec<TestNode>) -> Vec<Vec<RaftData>> {
+        let mut received = Vec::new();
+        for test_node in test_nodes {
+            received.push(test_node.committed_rx.recv().await.unwrap());
+        }
+        received
+    }
+
+    fn expected_commited(
+        test_nodes: &Vec<TestNode>,
+        expected: &Vec<RaftData>,
+    ) -> Vec<Vec<RaftData>> {
+        test_nodes.iter().map(|_| expected.clone()).collect()
+    }
+
+    fn test_configs(num_peers: u64) -> (HashMap<u64, usize>, Vec<TestNode>) {
+        let peers: Vec<u64> = (1..num_peers + 1).collect();
+        let test_nodes: Vec<_> = peers
+            .iter()
+            .map(|peer_id| test_config(*peer_id, &peers))
+            .collect();
+        let peer_to_indexes = peers
+            .iter()
+            .enumerate()
+            .map(|(idx, id)| (*id, idx))
+            .collect();
+        (peer_to_indexes, test_nodes)
+    }
+
+    fn test_config(peer_id: u64, peers: &Vec<u64>) -> TestNode {
+        let (msg_tx, msg_rx) = mpsc::channel(100);
+        let (committed_tx, committed_rx) = mpsc::channel(100);
+        let (msg_out_tx, msg_out_rx) = mpsc::channel(100);
+
+        let raft_config = RaftConfig {
+            cfg: Config {
+                id: peer_id,
+                peers: peers.clone(),
+                ..Default::default()
+            },
             msg_rx,
             committed_tx,
+            msg_out_tx,
             timeout_duration: Duration::from_millis(1),
-        })
-        .await;
-    });
+        };
 
-    // Send a proposal and wait for it to be commited
-    let proposed_data = vec![17];
-    msg_tx
-        .send(Msg::Propose {
-            data: proposed_data.clone(),
-        })
-        .await
-        .unwrap();
-    let commited_data = committed_rx.recv().await.unwrap();
-    assert_eq!(commited_data, vec![proposed_data]);
-
-    // Close raft loop
-    drop(msg_tx);
-
-    // Wait for the process to complete i.e dropping the msg_tx.
-    let (result,) = tokio::join!(main_handle);
-    result.unwrap();
+        TestNode {
+            raft_config: Some(raft_config),
+            msg_tx,
+            committed_rx,
+            msg_out_rx: Some(msg_out_rx),
+        }
+    }
 }
