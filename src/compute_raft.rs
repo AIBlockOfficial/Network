a9c52699eca092fa0b3c253cc84efb165235cbe2
Jean-Philippe DUFRAIGNE <jp@dufraigne.eu>
2020-11-30 15:39:05 +0100

    Clarify consensused fields
    Extract all consensused info in their own sub struct.
    
    Test:
    cargo test

--- src/compute_raft.rs
+++ src/compute_raft.rs
@@ -23,6 +23,22 @@ pub enum ComputeRaftItem {
     DruidTransactions(Vec<BTreeMap<String, Transaction>>),
 }
 
+// All fields that are consensused between the RAFT group.
+// These fields need to be written and read from a committed log event.
+#[derive(Clone, Debug, Default)]
+pub struct ComputeConsensused {
+    /// Committed transaction pool.
+    tx_pool: BTreeMap<String, Transaction>,
+    /// Committed DRUID transactions.
+    tx_druid_pool: Vec<BTreeMap<String, Transaction>>,
+    /// Header to use for next block if ready to generate.
+    tx_previous_hash: Option<String>,
+    /// Current block ready to mine (consensused).
+    current_block: Option<Block>,
+    /// All transactions present in current_block (consensused).
+    current_block_tx: BTreeMap<String, Transaction>,
+}
+
 /// Consensused Compute fields and consensus managment.
 pub struct ComputeRaft {
     // false if RAFT is bypassed.
@@ -39,16 +55,8 @@ pub struct ComputeRaft {
     peer_addr: HashMap<u64, SocketAddr>,
     /// Collection of the peer this node is responsible to connect to.
     compute_peers_to_connect: Vec<SocketAddr>,
-    /// Committed transaction pool.
-    tx_pool: BTreeMap<String, Transaction>,
-    /// Committed DRUID transactions.
-    tx_druid_pool: Vec<BTreeMap<String, Transaction>>,
-    /// Header to use for next block if ready to generate.
-    tx_previous_hash: Option<String>,
-    /// Current block ready to mine (consensused).
-    current_block: Option<Block>,
-    /// All transactions present in current_block (consensused).
-    current_block_tx: BTreeMap<String, Transaction>,
+    /// Consensused fields.
+    consensused: ComputeConsensused,
     /// Local transaction pool.
     local_tx_pool: BTreeMap<String, Transaction>,
     /// Local DRUID transaction pool.
@@ -102,11 +110,7 @@ impl ComputeRaft {
             committed_rx: Arc::new(Mutex::new(raft_channels.committed_rx)),
             peer_addr,
             compute_peers_to_connect,
-            tx_pool: BTreeMap::new(),
-            tx_druid_pool: Vec::new(),
-            tx_previous_hash: None,
-            current_block: None,
-            current_block_tx: BTreeMap::new(),
+            consensused: Default::default(),
             local_tx_pool: BTreeMap::new(),
             local_tx_druid_pool: Vec::new(),
             local_last_block_hash: "".to_string(),
@@ -137,22 +141,22 @@ impl ComputeRaft {
         for data in raft_data.drain(..) {
             match deserialize::<ComputeRaftItem>(&data) {
                 Ok(ComputeRaftItem::Transactions(mut transactions)) => {
-                    self.tx_pool.append(&mut transactions);
+                    self.consensused.tx_pool.append(&mut transactions);
                 }
                 Ok(ComputeRaftItem::DruidTransactions(mut transactions)) => {
-                    self.tx_druid_pool.append(&mut transactions);
+                    self.consensused.tx_druid_pool.append(&mut transactions);
                 }
                 Ok(ComputeRaftItem::Block(previous_hash)) => {
                     // TODO: Ensure that tx_pool & tx_druid_pool are not populated further
                     //       before generating the block.
-                    self.tx_previous_hash = Some(previous_hash);
+                    self.consensused.tx_previous_hash = Some(previous_hash);
                 }
                 Err(error) => warn!(?error, "ComputeRaftItem-deserialize"),
             }
         }
 
-        if self.tx_previous_hash.is_some()
-            && (!self.tx_pool.is_empty() || !self.tx_druid_pool.is_empty())
+        if self.consensused.tx_previous_hash.is_some()
+            && (!self.consensused.tx_pool.is_empty() || !self.consensused.tx_druid_pool.is_empty())
         {
             Some(())
         } else {
@@ -193,18 +197,18 @@ impl ComputeRaft {
     }
 
     pub fn commited_tx_pool(&mut self) -> &mut BTreeMap<String, Transaction> {
-        &mut self.tx_pool
+        &mut self.consensused.tx_pool
     }
 
     pub fn tx_pool_len(&self) -> usize {
-        self.tx_pool.len() + self.local_tx_pool.len()
+        self.consensused.tx_pool.len() + self.local_tx_pool.len()
     }
 
     pub fn append_to_tx_pool(&mut self, mut transactions: BTreeMap<String, Transaction>) {
         if self.use_raft {
             self.local_tx_pool.append(&mut transactions);
         } else {
-            self.tx_pool.append(&mut transactions);
+            self.consensused.tx_pool.append(&mut transactions);
         }
     }
 
@@ -212,24 +216,24 @@ impl ComputeRaft {
         if self.use_raft {
             self.local_last_block_hash = value;
         } else {
-            self.tx_previous_hash = Some(value);
+            self.consensused.tx_previous_hash = Some(value);
         }
     }
 
     pub fn get_committed_previous_hash(&self) -> &Option<String> {
-        &self.tx_previous_hash
+        &self.consensused.tx_previous_hash
     }
 
     pub fn append_to_tx_druid_pool(&mut self, transactions: BTreeMap<String, Transaction>) {
         if self.use_raft {
             self.local_tx_druid_pool.push(transactions);
         } else {
-            self.tx_druid_pool.push(transactions);
+            self.consensused.tx_druid_pool.push(transactions);
         }
     }
 
     pub fn commited_tx_druid_pool(&mut self) -> &mut Vec<BTreeMap<String, Transaction>> {
-        &mut self.tx_druid_pool
+        &mut self.consensused.tx_druid_pool
     }
 
     pub fn set_committed_mining_block(
@@ -237,17 +241,17 @@ impl ComputeRaft {
         block: Block,
         block_tx: BTreeMap<String, Transaction>,
     ) {
-        self.current_block = Some(block);
-        self.current_block_tx = block_tx;
+        self.consensused.current_block = Some(block);
+        self.consensused.current_block_tx = block_tx;
     }
 
     pub fn get_mining_block(&self) -> &Option<Block> {
-        &self.current_block
+        &self.consensused.current_block
     }
 
     pub fn take_mining_block(&mut self) -> (Block, BTreeMap<String, Transaction>) {
-        let block = std::mem::take(&mut self.current_block).unwrap();
-        let block_tx = std::mem::take(&mut self.current_block_tx);
+        let block = std::mem::take(&mut self.consensused.current_block).unwrap();
+        let block_tx = std::mem::take(&mut self.consensused.current_block_tx);
         (block, block_tx)
     }
 
@@ -272,7 +276,7 @@ impl ComputeRaft {
         block: &mut Block,
         block_tx: &mut BTreeMap<String, Transaction>,
     ) {
-        for mut txs in self.tx_druid_pool.drain(..) {
+        for mut txs in self.consensused.tx_druid_pool.drain(..) {
             // Process a set of transactions from a single DRUID droplet.
             block.transactions.extend(txs.keys().cloned());
             block_tx.append(&mut txs);
@@ -287,10 +291,10 @@ impl ComputeRaft {
     ) {
         if block_tx.len() == 0 {
             let mut txs = {
-                let mut txs = std::mem::take(&mut self.tx_pool);
+                let mut txs = std::mem::take(&mut self.consensused.tx_pool);
                 if let Some(max_key) = txs.keys().nth(BLOCK_SIZE_IN_TX).cloned() {
                     // Set back overflowing transactions.
-                    self.tx_pool = txs.split_off(&max_key);
+                    self.consensused.tx_pool = txs.split_off(&max_key);
                 }
                 txs
             };
@@ -302,7 +306,7 @@ impl ComputeRaft {
 
     fn update_block_header(&mut self, block: &mut Block) {
         let previous_hash = {
-            let previous_hash = std::mem::take(&mut self.tx_previous_hash);
+            let previous_hash = std::mem::take(&mut self.consensused.tx_previous_hash);
             if self.use_raft {
                 previous_hash.unwrap()
             } else {
