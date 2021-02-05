4f57f68eac719b81b0c16b7beb7657d940f917f9
Myrin Naidoo <myrinnnaidoo5@gmail.com>
2021-02-05 16:36:07 +0200

    Double Checked interfaces.rs

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -63,6 +63,7 @@ impl Default for ProofOfWorkBlock {
 }
 
 impl ProofOfWorkBlock {
+    ///Proof of work block constructor
     pub fn new() -> Self {
         ProofOfWorkBlock {
             nonce: Vec::new(),
