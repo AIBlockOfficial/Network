cf80116c666f843055cca1f5d044e193ef8b7f4f
Jean-Philippe DUFRAIGNE <jp@dufraigne.eu>
2021-03-10 15:00:52 +0100

    Complete package/deploy scripts
    We can now do basic deployment tasks:
     - Package
     - Deploy
     - Get log files
    Package binaries and configurations:
     - All files
     - Just configs
     - configs and json (initial block)
    
    Deploy packaged and start network:
     - if requested: Check ssh connection with all nodes
     - if requested: Get existing log files
     - Create run scripts for each nodes
     - if requested: Stage: Upload package binaries to all node in network (can be full binary or config update only if full binary was deployed before).
     - if requested: Upload upload scripts to each nodes and run them in screen
    
    The remote scripts do:
     - kill previous node
     - optionally (enabled now) delete databases
     - extract update
     - start new nodes
     - create kill scripts
     - wait until it is killed
    
    Test:
    (run before any commit compared to develop branch)
    src/bin/node_settings_package.sh
    => Create packages
    
    src/bin/node_settings_aws_deploy.sh stage_all target/release/znp-8327ae027712faffd0b2b56482d81bc21e78643c.tar.gz complete_deploy_all no_check_connection get_existing_logs
    => deploy change
    
    For phased staging if slow upload connection:
    src/bin/node_settings_aws_deploy.sh stage_first target/release/znp-8327ae027712faffd0b2b56482d81bc21e78643c.tar.gz
    => deploy full change to first node.
    
    ssh -i ssh -i ~/.ssh/Zenotta-Node.pem ubuntu@ec2-52-40-82-170.us-west-2.compute.amazonaws.com
    node_settings_aws_deploy.sh stage_others target/release/znp-8327ae027712faffd0b2b56482d81bc21e78643c.tar.gz
    exit
    
    src/bin/node_settings_aws_deploy.sh stage_all target/release/znp-8327ae027712faffd0b2b56482d81bc21e78643c.configs.tar.gz complete_deploy_all no_check_connection get_existing_logs
    => deploy config change only

---
+++ src/bin/node_settings_aws_run_one.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+set -x
+NODE_TYPE=$1
+INDEX=$2
+COMPUTE_INDEX=$3
+LOG_LEVEL=$4
+PACKAGE_NAME=$5
+START_CLEAN_DB=$6
+NODE_CONFIG=src/bin/node_settings_aws.toml
+NODE_INIT_BLOCK=src/bin/initial_block.json
+set +x
+
+echo " "
+echo "//-----------------------------//"
+echo "Kill existing node"
+echo "//-----------------------------//"
+echo " "
+set -v
+sh ./znp/kill_node.sh
+set +v
+
+echo " "
+echo "//-----------------------------//"
+echo "Extract package if $PACKAGE_NAME != keep_file_restart_only"
+echo "//-----------------------------//"
+echo " "
+if [ "$PACKAGE_NAME" != "keep_file_restart_only" ]
+then
+    set -v
+    mkdir ./znp
+    tar -xvzf $PACKAGE_NAME -C znp
+    set +v
+fi
+
+echo " "
+echo "//-----------------------------//"
+echo "Move to znp path"
+echo "//-----------------------------//"
+echo " "
+set -v
+cd znp
+set +v
+
+echo " "
+echo "//-----------------------------//"
+echo "Check if need start clean db $START_CLEAN_DB==start_with_clean_db"
+echo "//-----------------------------//"
+echo " "
+if [ "$START_CLEAN_DB" = "start_with_clean_db" ]
+then
+    set -v
+    echo "Delete databases"
+    rm -rf src/db/db/test.* src/wallet/wallet/test.*
+    set +v
+fi
+
+echo " "
+echo "//-----------------------------//"
+echo "Running $NODE_TYPE node $INDEX for $NODE_CONFIG"
+echo "//-----------------------------//"
+echo " "
+
+if [ "$NODE_TYPE" = "storage" ]
+then
+    set -x
+    RUST_LOG="$LOG_LEVEL" target/release/storage --config=$NODE_CONFIG --index=$INDEX > storage_$INDEX.log 2>&1 &
+    n0=$!
+    set +x
+elif [ "$NODE_TYPE" = "compute" ]
+then
+    set -x
+    RUST_LOG="$LOG_LEVEL" target/release/compute --config=$NODE_CONFIG --initial_block_config=$NODE_INIT_BLOCK --index=$INDEX > compute_$INDEX.log 2>&1 &
+    n0=$!
+    set +x
+elif [ "$NODE_TYPE" = "miner" ]
+then
+    set -x
+    RUST_LOG="$LOG_LEVEL" target/release/miner --config=$NODE_CONFIG --index=$INDEX --compute_index=$COMPUTE_INDEX > miner_$INDEX.log 2>&1 &
+    n0=$!
+    set +x
+elif [ "$NODE_TYPE" = "user" ]
+then
+    set -x
+    RUST_LOG="$LOG_LEVEL" target/release/user --config=$NODE_CONFIG --initial_block_config=$NODE_INIT_BLOCK --index=$INDEX --compute_index=$COMPUTE_INDEX > user_$INDEX.log 2>&1 &
+    n0=$!
+    set +x
+fi
+
+echo "kill $n0" > kill_$NODE_TYPE.sh
+echo "kill $n0" > kill_node.sh
+echo "Started $NODE_TYPE node at $n0, kill with kill_$NODE_TYPE.sh or kill_node.sh"
+set +x
+set +v
+
+# Make sure the command do not complete so node process not killed
+# when screen exit
+echo "wait $n0" 
+wait $n0
--- src/bin/node_settings_package.sh
+++ src/bin/node_settings_package.sh
@@ -12,20 +12,15 @@ then
 fi
 echo " "
 echo "//-----------------------------//"
-echo "Get configuration files"
-echo "//-----------------------------//"
-echo " "
-rm -rf target/release/src/bin
-mkdir -p target/release/src/bin
-cp src/bin/*.toml src/bin/*.json src/bin/*.sh target/release/src/bin
-echo " "
-echo "//-----------------------------//"
 echo "Create tar.gz file"
 echo "//-----------------------------//"
 echo " "
 
 COMMIT=`git rev-parse HEAD`
-cd target/release
-tar -cvzf znp-$COMMIT.tar.gz compute  miner  storage  user src
+tar -cvzf target/release/znp-$COMMIT.tar.gz target/release/compute  target/release/miner  target/release/storage  target/release/user src/bin/*.toml src/bin/*.json src/bin/*.sh
+tar -cvzf target/release/znp-$COMMIT.configs.tar.gz src/bin/*.toml src/bin/*.json src/bin/*.sh
+tar -cvzf target/release/znp-$COMMIT.light-config.tar.gz src/bin/*.toml src/bin/*.sh
 
 echo "Complete package at target/release/znp-$COMMIT.tar.gz"
+echo "Complete config package at target/release/znp-$COMMIT.configs.tar.gz"
+echo "Complete light config package at target/release/znp-$COMMIT.light-config.tar.gz"
