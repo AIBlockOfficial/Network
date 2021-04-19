3af36e609ab67529055773c71b4de6c156dee758
Jean-Philippe DUFRAIGNE <jp@dufraigne.eu>
2021-04-19 15:45:18 +0200

    Upgrade main storage db and run with binary
    -Add upgrade processing to upgrade binary
    -Add all needed deserialization for 0.2.0 version and conversion.
    -Process upgrade finding all blocks/transactions and last block stored for restart.
    -Provide a mean for restarting Raft from scratch for storage.
    
    Test:
    New automatic tests
    cargo test
    cargo clippy
    
    Validate binary:
    znp$ rm -rf src/db/db/test.* src/wallet/wallet/test.*; tar -xzf src/bin/dbs_v_0_2_0.tar.gz
    znp$ cargo run --bin upgrade -- --type all --processing upgrade
    => Upgrade with config [("compute", Test(0)), ("storage", Test(0)), ("user", Test(1000)), ("user", Test(1001)), ("miner", Test(0)), ("miner", Test(1))]
    => Upgrade Database compute, Test(0)
    => Done Upgrade Database compute, Test(0)
    => Upgrade Database storage, Test(0)
    => Done Upgrade Database storage, Test(0)
    => Upgrade Database user, Test(1000)
    => Upgrade error, aborting: ConfigError("Not implemented for this type")
    
    znp$ cargo run --bin upgrade -- --type compute --processing upgrade
    => Upgrade with config [("compute", Test(0))]
    => Upgrade Database compute, Test(0)
    => Upgrade error, aborting: DbError(SimpleDbError("DB Version mismatch"))
    
    znp$ rm -rf src/db/db/test.* src/wallet/wallet/test.*; tar -xzf src/bin/dbs_v_0_2_0.tar.gz 
    znp$ cargo run --bin upgrade -- --type compute --processing upgrade
    => Upgrade with config [("compute", Test(0))]
    => Upgrade Database compute, Test(0)
    => Done Upgrade Database compute, Test(0)
    
    znp$ cargo run --bin upgrade -- --type storage --processing upgrade
    => Upgrade with config [("storage", Test(0))]
    => Upgrade Database storage, Test(0)
    => Done Upgrade Database storage, Test(0)

--- src/upgrade/mod.rs
+++ src/upgrade/mod.rs
@@ -8,19 +8,21 @@ mod tests;
 #[cfg(test)]
 mod tests_last_version_db;
 
-use crate::compute;
 use crate::configurations::DbMode;
 use crate::constants::{DB_PATH, DB_VERSION_KEY, NETWORK_VERSION_SERIALIZED, WALLET_PATH};
 use crate::db_utils::{
     new_db_no_check_version, new_db_with_version, SimpleDb, SimpleDbError, SimpleDbSpec,
     SimpleDbWriteBatch, DB_COL_DEFAULT,
 };
-use bincode::deserialize;
+use crate::interfaces::BlockStoredInfo;
+use crate::{compute, storage};
+use bincode::{deserialize, serialize};
 use frozen_last_version as old;
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 use std::error::Error;
 use std::fmt;
 use std::net::SocketAddr;
+use tracing::error;
 
 pub const DB_SPEC_INFOS: &[DbSpecInfo] = &[
     DbSpecInfo {
@@ -138,6 +140,106 @@ pub fn upgrade_compute_db_batch<'a>(
     Ok(batch)
 }
 
+/// Upgrade DB: New column are added at begining of upgrade and old one removed at the end.
+pub fn get_upgrade_storage_db(db_mode: DbMode, old_db: Option<SimpleDb>) -> Result<SimpleDb> {
+    let spec = &old::storage::DB_SPEC;
+    let version = old::constants::NETWORK_VERSION_SERIALIZED;
+    let mut db = new_db_with_version(db_mode, spec, version, old_db)?;
+
+    db.upgrade_create_missing_cf(storage::DB_COL_INTERNAL)?;
+    db.upgrade_create_missing_cf(storage::DB_COL_BC_ALL)?;
+    db.upgrade_create_missing_cf(storage::DB_COL_BC_NOW)?;
+    db.upgrade_create_missing_cf(storage::DB_COL_BC_V0_2_0)?;
+    Ok(db)
+}
+
+/// Upgrade DB: upgrade ready given db  .
+pub fn upgrade_storage_db(mut db: SimpleDb) -> Result<SimpleDb> {
+    let batch = upgrade_storage_db_batch(&db, db.batch_writer())?.done();
+    db.write(batch)?;
+    Ok(db)
+}
+
+/// Upgrade DB: all columns new and old are expected to be opened
+pub fn upgrade_storage_db_batch<'a>(
+    db: &SimpleDb,
+    mut batch: SimpleDbWriteBatch<'a>,
+) -> Result<SimpleDbWriteBatch<'a>> {
+    batch.put_cf(DB_COL_DEFAULT, DB_VERSION_KEY, NETWORK_VERSION_SERIALIZED);
+
+    let mut max_block = None;
+    for (key, value) in db.iter_cf_clone(DB_COL_DEFAULT) {
+        batch.delete_cf(DB_COL_DEFAULT, &key);
+
+        if is_transaction_key(&key) {
+            let _: old::naom::Transaction = tracked_deserialize("Tx deserialize", &key, &value)?;
+            storage::put_to_block_chain_at(&mut batch, storage::DB_COL_BC_V0_2_0, &key, &value);
+        } else if is_block_key(&key) {
+            let stored_block: old::naom::StoredSerializingBlock =
+                tracked_deserialize("Block deserialize", &key, &value)?;
+            let b_num = stored_block.block.header.b_num;
+
+            max_block = std::cmp::max(max_block, Some((b_num, key.clone())));
+            storage::put_to_block_chain_at(&mut batch, storage::DB_COL_BC_V0_2_0, &key, &value);
+        } else {
+            let e = UpgradeError::ConfigError("Unexpected key");
+            return Err(log_key_value_error(e, "Unexpected key", &key, &value));
+        }
+    }
+
+    if let Some((_, key)) = max_block {
+        let value = db.get_cf(DB_COL_DEFAULT, &key)?;
+        let value = value.ok_or(UpgradeError::ConfigError("Missing last block"))?;
+        let stored_block: old::naom::StoredSerializingBlock =
+            tracked_deserialize("Block deserialize", &key, &value)?;
+
+        let mut mining_transactions = BTreeMap::new();
+        for (_, (tx_hash, _)) in stored_block.mining_tx_hash_and_nonces {
+            let value = db.get_cf(DB_COL_DEFAULT, &tx_hash)?;
+            let value = value.ok_or(UpgradeError::ConfigError("Missing mining tx"))?;
+            let tx: old::naom::Transaction = tracked_deserialize("Tx deserialize", &key, &value)?;
+
+            let tx = old::convert_transaction(tx);
+            mining_transactions.insert(tx_hash, tx);
+        }
+
+        let block_hash =
+            String::from_utf8(key).map_err(|_| UpgradeError::ConfigError("Non UTF-8 block key"))?;
+
+        let header = stored_block.block.header;
+
+        let last_block_stored = BlockStoredInfo {
+            block_hash,
+            block_num: header.b_num,
+            merkle_hash: header.merkle_root_hash,
+            nonce: header.nonce,
+            mining_transactions,
+            shutdown: false,
+        };
+        let last_block_stored_ser = serialize(&last_block_stored)?;
+        batch.put_cf(
+            storage::DB_COL_INTERNAL,
+            storage::LAST_BLOCK_STORED_INIT_KEY,
+            &last_block_stored_ser,
+        );
+    } else {
+        return Err(UpgradeError::ConfigError("No last block"));
+    }
+
+    Ok(batch)
+}
+
+/// whether it is a transaction key
+pub fn is_transaction_key(key: &[u8]) -> bool {
+    // special genesis block transactions had 6 digits and missed prefix
+    key.get(0) == Some(&b'g') || key.len() == 6
+}
+
+/// whether it is a block key
+pub fn is_block_key(key: &[u8]) -> bool {
+    key.len() == 64
+}
+
 /// Open a database for dump doing no checks on validity
 pub fn get_db_to_dump_no_checks(
     db_mode: DbMode,
@@ -173,4 +275,45 @@ fn to_u8_array_literal(value: &[u8]) -> String {
         }
     }
     result
+}
+
+fn log_key_value_error<E: fmt::Debug>(e: E, info: &str, key: &[u8], value: &[u8]) -> E {
+    let log_key = to_u8_array_literal(key);
+    let log_value = to_u8_array_literal(value);
+    error!("{}: {:?} \"{}\" -> \"{}\"", info, e, log_key, log_value);
+    e
+}
+
+fn tracked_deserialize<'a, T: serde::Deserialize<'a>>(
+    tag: &str,
+    key: &[u8],
+    value: &'a [u8],
+) -> Result<T> {
+    Ok(match deserialize(value) {
+        Ok(TrackedSer(v)) => Ok(v),
+        Err(e) => Err(log_key_value_error(e, tag, key, value)),
+    }?)
+}
+
+/// Provide tracked deserialization for better error logs
+pub struct TrackedSer<T>(T);
+
+impl<'a, T> serde::Deserialize<'a> for TrackedSer<T>
+where
+    T: serde::Deserialize<'a>,
+{
+    fn deserialize<D: serde::Deserializer<'a>>(
+        deserializer: D,
+    ) -> std::result::Result<Self, D::Error> {
+        let result: std::result::Result<T, _> = serde_path_to_error::deserialize(deserializer);
+        match result {
+            Ok(v) => Ok(TrackedSer(v)),
+            Err(err) => {
+                let path = err.path().to_string();
+                let err = err.into_inner();
+                error!("TrackedSer at: {} -> {:?}", path, err);
+                Err(err)
+            }
+        }
+    }
 }
--- src/upgrade/tests.rs
+++ src/upgrade/tests.rs
@@ -1,99 +1,146 @@
-use super::tests_last_version_db;
-use super::{get_upgrade_compute_db, old, upgrade_compute_db};
+use super::tests_last_version_db::{self, DbEntryType};
+use super::{
+    get_upgrade_compute_db, get_upgrade_storage_db, old, upgrade_compute_db, upgrade_storage_db,
+    UpgradeError,
+};
 use crate::configurations::{DbMode, ExtraNodeParams};
 use crate::constants::{DB_VERSION_KEY, NETWORK_VERSION_SERIALIZED};
 use crate::db_utils::{
     new_db, new_db_with_version, SimpleDb, SimpleDbError, SimpleDbSpec, DB_COL_DEFAULT,
 };
+use crate::interfaces::BlockStoredInfo;
 use crate::test_utils::{
     init_arc_node, init_instance_info, remove_all_node_dbs_in_info, NetworkConfig,
-    NetworkInstanceInfo, NodeType,
+    NetworkInstanceInfo, NetworkNodeInfo, NodeType,
 };
+use crate::{compute, storage};
 use std::collections::BTreeSet;
 use std::net::SocketAddr;
+use tracing::info;
 
 #[tokio::test(basic_scheduler)]
 async fn upgrade_compute_real_db() {
-    let config = complete_network_config(20000, false);
+    let config = real_db(complete_network_config(20000));
     let info = init_instance_info(&config);
     remove_all_node_dbs_in_info(&info);
 
-    upgrade_compute_common(config, info).await;
+    upgrade_common(config, info, "compute1").await;
 }
 
 #[tokio::test(basic_scheduler)]
 async fn upgrade_compute_in_memory() {
-    let config = complete_network_config(20010, true);
+    let config = complete_network_config(20010);
     let info = init_instance_info(&config);
-    upgrade_compute_common(config, info).await;
+    upgrade_common(config, info, "compute1").await;
 }
 
-async fn upgrade_compute_common(config: NetworkConfig, info: NetworkInstanceInfo) {
+#[tokio::test(basic_scheduler)]
+async fn upgrade_storage_in_memory() {
+    let config = complete_network_config(20020);
+    let info = init_instance_info(&config);
+    upgrade_common(config, info, "storage1").await;
+}
+
+async fn upgrade_common(config: NetworkConfig, info: NetworkInstanceInfo, name: &str) {
+    test_step_start();
+
     //
     // Arrange
     //
-    let db_mode = info.node_infos.get("compute1").as_ref().unwrap().db_mode;
-    let db = create_old_compute_db(db_mode);
+    let n_info = info.node_infos.get(name).unwrap();
+    let db = create_old_node_db(n_info);
 
     //
     // Act
     //
-    let db = get_upgrade_compute_db(db_mode, db.in_memory()).unwrap();
-    let db = upgrade_compute_db(db).unwrap();
-    let db = open_as_new_compute_db(db_mode, db.in_memory()).unwrap();
+    let db = get_upgrade_node_db(n_info, db.in_memory()).unwrap();
+    let db = upgrade_node_db(n_info, db).unwrap();
+    let db = open_as_new_node_db(n_info, db.in_memory()).unwrap();
 
-    let compute = {
+    let node = {
         let extra = ExtraNodeParams {
             db: db.in_memory(),
             ..Default::default()
         };
-        init_arc_node("compute1", &config, &info, extra).await
+        init_arc_node(name, &config, &info, extra).await
     };
-    let compute = compute.compute().unwrap().lock().await;
-    let actual_req_list = compute.get_request_list();
 
     //
     // Assert
     //
-    let expected_req_list: BTreeSet<SocketAddr> =
-        std::iter::once("127.0.0.1:12340".parse().unwrap()).collect();
-    assert_eq!(actual_req_list, &expected_req_list);
+    match n_info.node_type {
+        NodeType::Compute => {
+            let compute = node.compute().unwrap().lock().await;
+
+            let expected_req_list: BTreeSet<SocketAddr> =
+                std::iter::once("127.0.0.1:12340".parse().unwrap()).collect();
+            assert_eq!(compute.get_request_list(), &expected_req_list);
+        }
+        NodeType::Storage => {
+            let storage = node.storage().unwrap().lock().await;
+
+            {
+                let mut expected = Vec::new();
+                let mut actual = Vec::new();
+                for (_, k, v) in tests_last_version_db::STORAGE_DB_V0_2_0 {
+                    expected.push(Some(v.to_vec()));
+                    actual.push(storage.get_stored_value(k));
+                }
+                assert_eq!(actual, expected);
+                assert_eq!(storage.get_stored_values_count(), expected.len());
+                assert_eq!(
+                    storage.get_last_block_stored(),
+                    &Some(get_expected_last_block_stored())
+                );
+            }
+        }
+        _ => unimplemented!(),
+    }
 }
 
 #[tokio::test(basic_scheduler)]
 async fn open_upgrade_started_compute_real_db() {
-    let config = complete_network_config(20100, false);
+    let config = real_db(complete_network_config(20100));
     let info = init_instance_info(&config);
     remove_all_node_dbs_in_info(&info);
 
-    open_upgrade_started_compute_common(info).await;
+    open_upgrade_started_compute_common(info, "compute1").await;
 }
 
 #[tokio::test(basic_scheduler)]
 async fn open_upgrade_started_compute_in_memory() {
-    let config = complete_network_config(20110, true);
+    let config = complete_network_config(20110);
     let info = init_instance_info(&config);
-    open_upgrade_started_compute_common(info).await;
+    open_upgrade_started_compute_common(info, "compute1").await;
 }
 
-async fn open_upgrade_started_compute_common(info: NetworkInstanceInfo) {
+#[tokio::test(basic_scheduler)]
+async fn open_upgrade_started_storage_in_memory() {
+    let config = complete_network_config(20120);
+    let info = init_instance_info(&config);
+    open_upgrade_started_compute_common(info, "storage1").await;
+}
+
+async fn open_upgrade_started_compute_common(info: NetworkInstanceInfo, name: &str) {
+    test_step_start();
+
     //
     // Arrange
     //
-    let db_mode = info.node_infos.get("compute1").as_ref().unwrap().db_mode;
-    let db = create_old_compute_db(db_mode);
+    let n_info = info.node_infos.get(name).unwrap();
+    let db = create_old_node_db(n_info);
 
     //
     // Act
     //
-    let err_new_1 = open_as_new_compute_db(db_mode, db.cloned_in_memory()).err();
-    let db = open_as_old_compute_db(db_mode, db.in_memory()).unwrap();
+    let err_new_1 = open_as_new_node_db(n_info, db.cloned_in_memory()).err();
+    let db = open_as_old_node_db(n_info, db.in_memory()).unwrap();
 
-    let db = get_upgrade_compute_db(db_mode, db.in_memory()).unwrap();
-    let db = open_as_old_compute_db(db_mode, db.in_memory()).unwrap();
-    let db = get_upgrade_compute_db(db_mode, db.in_memory()).unwrap();
+    let db = get_upgrade_node_db(n_info, db.in_memory()).unwrap();
+    let db = open_as_old_node_db(n_info, db.in_memory()).unwrap();
+    let db = get_upgrade_node_db(n_info, db.in_memory()).unwrap();
 
-    let err_new_2 = open_as_new_compute_db(db_mode, db.cloned_in_memory()).err();
+    let err_new_2 = open_as_new_node_db(n_info, db.cloned_in_memory()).err();
 
     //
     // Assert
@@ -102,13 +149,27 @@ async fn open_upgrade_started_compute_common(info: NetworkInstanceInfo) {
     assert!(err_new_2.is_some());
 }
 
-fn create_old_compute_db(db_mode: DbMode) -> SimpleDb {
-    let spec = &old::compute::DB_SPEC;
+fn create_old_node_db(info: &NetworkNodeInfo) -> SimpleDb {
+    let (spec, entries) = match info.node_type {
+        NodeType::Compute => (
+            &old::compute::DB_SPEC,
+            &tests_last_version_db::COMPUTE_DB_V0_2_0,
+        ),
+        NodeType::Storage => (
+            &old::storage::DB_SPEC,
+            &tests_last_version_db::STORAGE_DB_V0_2_0,
+        ),
+        _ => unimplemented!(),
+    };
+    create_old_db(spec, info.db_mode, entries)
+}
+
+fn create_old_db(spec: &SimpleDbSpec, db_mode: DbMode, entries: &[DbEntryType]) -> SimpleDb {
     let mut db = new_db(db_mode, spec, None);
 
     let mut batch = db.batch_writer();
     batch.delete_cf(DB_COL_DEFAULT, DB_VERSION_KEY);
-    for (_column, key, value) in tests_last_version_db::COMPUTE_DB_V0_2_0 {
+    for (_column, key, value) in entries {
         batch.put_cf(DB_COL_DEFAULT, key, value);
     }
     let batch = batch.done();
@@ -117,30 +178,66 @@ fn create_old_compute_db(db_mode: DbMode) -> SimpleDb {
     db
 }
 
-fn open_as_old_compute_db(
-    db_mode: DbMode,
+fn open_as_old_node_db(
+    info: &NetworkNodeInfo,
     old_db: Option<SimpleDb>,
 ) -> Result<SimpleDb, SimpleDbError> {
-    let spec = &old::compute::DB_SPEC;
     let version = old::constants::NETWORK_VERSION_SERIALIZED;
-    new_db_with_version(db_mode, spec, version, old_db)
+    let spec = match info.node_type {
+        NodeType::Compute => &old::compute::DB_SPEC,
+        NodeType::Storage => &old::storage::DB_SPEC,
+        _ => unimplemented!(),
+    };
+    new_db_with_version(info.db_mode, spec, version, old_db)
 }
 
-fn open_as_new_compute_db(
-    db_mode: DbMode,
+fn open_as_new_node_db(
+    info: &NetworkNodeInfo,
     old_db: Option<SimpleDb>,
 ) -> Result<SimpleDb, SimpleDbError> {
-    let spec = &old::compute::DB_SPEC;
     let version = Some(NETWORK_VERSION_SERIALIZED);
-    new_db_with_version(db_mode, spec, version, old_db)
+    let spec = match info.node_type {
+        NodeType::Compute => &compute::DB_SPEC,
+        NodeType::Storage => &storage::DB_SPEC,
+        _ => unimplemented!(),
+    };
+    new_db_with_version(info.db_mode, spec, version, old_db)
 }
 
-fn complete_network_config(initial_port: u16, in_memory_db: bool) -> NetworkConfig {
+pub fn get_upgrade_node_db(
+    info: &NetworkNodeInfo,
+    old_db: Option<SimpleDb>,
+) -> Result<SimpleDb, UpgradeError> {
+    match info.node_type {
+        NodeType::Compute => get_upgrade_compute_db(info.db_mode, old_db),
+        NodeType::Storage => get_upgrade_storage_db(info.db_mode, old_db),
+        _ => unimplemented!(),
+    }
+}
+
+pub fn upgrade_node_db(info: &NetworkNodeInfo, db: SimpleDb) -> Result<SimpleDb, UpgradeError> {
+    match info.node_type {
+        NodeType::Compute => upgrade_compute_db(db),
+        NodeType::Storage => upgrade_storage_db(db),
+        _ => unimplemented!(),
+    }
+}
+
+//
+// Test helpers
+//
+
+fn test_step_start() {
+    let _ = tracing_subscriber::fmt::try_init();
+    info!("Test Step start");
+}
+
+fn complete_network_config(initial_port: u16) -> NetworkConfig {
     NetworkConfig {
         initial_port,
         compute_raft: false,
         storage_raft: false,
-        in_memory_db,
+        in_memory_db: true,
         compute_partition_full_size: 1,
         compute_minimum_miner_pool_len: 1,
         nodes: vec![
@@ -159,10 +256,57 @@ fn complete_network_config(initial_port: u16, in_memory_db: bool) -> NetworkConf
     }
 }
 
+fn real_db(mut config: NetworkConfig) -> NetworkConfig {
+    config.in_memory_db = false;
+    config
+}
+
 fn get_static_column(spec: SimpleDbSpec, name: &str) -> &'static str {
     [DB_COL_DEFAULT]
         .iter()
         .chain(spec.columns.iter())
         .find(|sn| **sn == name)
         .unwrap()
+}
+
+fn get_expected_last_block_stored() -> BlockStoredInfo {
+    use naom::primitives::{
+        asset::{Asset, TokenAmount},
+        transaction::{Transaction, TxIn, TxOut},
+    };
+    use naom::script::{lang::Script, StackEntry};
+
+    BlockStoredInfo {
+        block_hash: "f628017bb00472a33a5070bce18ef68320c558f999350e1a3164f319ba9b5c00".to_owned(),
+        block_num: 2,
+        nonce: Vec::new(),
+        merkle_hash: "24c87c26cf5233f59ffe9b3f8f19cd7e1cdcf871dafb2e3e800e15cf155da944".to_owned(),
+        mining_transactions: std::iter::once((
+            "g567775cc21b9647014a6b7959919911".to_owned(),
+            Transaction {
+                inputs: vec![TxIn {
+                    previous_out: None,
+                    script_signature: Script {
+                        stack: vec![StackEntry::Num(2)],
+                    },
+                }],
+                outputs: vec![TxOut {
+                    value: Some(Asset::Token(TokenAmount(7510184))),
+                    amount: TokenAmount(7510184),
+                    locktime: 0,
+                    drs_block_hash: None,
+                    drs_tx_hash: None,
+                    script_public_key: Some("6e8f40e652d5f26c6e65180602a289a2".to_owned()),
+                }],
+                version: 0,
+                druid: None,
+                druid_participants: None,
+                expect_value: None,
+                expect_value_amount: None,
+                expect_address: None,
+            },
+        ))
+        .collect(),
+        shutdown: false,
+    }
 }
