AIBlockOfficial/Network

commit 793189a58b5396dcd1010be6e5adeabc8697ddf7

Clean up user tx auto generation
Move autogneration properly as part of the user code.
This simplify the event handling avoiding special case.

Test:
cargo test
cargo clippy
src/bin/node_settings_run.sh
=> transactions as expected

src/configurations.rs
@@ -145,11 +145,13 @@ pub struct UserNodeConfig {
     pub user_wallet_seeds: Vec<Vec<WalletTxSpec>>,
     /// Option of the passphrase used for encryption
     pub passphrase: Option<String>,
+    /// Configuration options for auto generating transactions for test
+    pub test_auto_gen_setup: UserAutoGenTxSetup,
 }
 
 /// Configuration option for setup of user node
 #[derive(Default, Debug, Clone, Deserialize)]
-pub struct UserNodeSetup {
+pub struct UserAutoGenTxSetup {
     /// Transaction seeds, for each users
     pub user_initial_transactions: Vec<Vec<WalletTxSpec>>,
     /// How many transaction to group in each requests

src/tests.rs
@@ -1,7 +1,7 @@
 //! Test suite for the network functions.
 
 use crate::compute::ComputeNode;
-use crate::configurations::{TxOutSpec, UserNodeSetup, UtxoSetSpec, WalletTxSpec};
+use crate::configurations::{TxOutSpec, UserAutoGenTxSetup, UtxoSetSpec, WalletTxSpec};
 use crate::constants::{BLOCK_PREPEND, SANC_LIST_TEST};
 use crate::interfaces::{
     BlockStoredInfo, CommonBlockInfo, ComputeRequest, MinedBlockExtraInfo, Response,
@@ -1203,22 +1203,18 @@ async fn main_loops_raft_1_node_common(
     //
     network_config.compute_seed_utxo =
         make_compute_seed_utxo(&[(seed_count, "000000")], initial_amount);
+    network_config.test_auto_gen_setup = UserAutoGenTxSetup {
+        user_initial_transactions: vec![(0..seed_wallet_count)
+            .map(|i| wallet_seed((i, "000000"), &initial_amount))
+            .collect()],
+        user_setup_tx_chunk_size: Some(5),
+        user_setup_tx_in_per_tx: Some(3),
+        user_setup_tx_max_count: tx_max_count,
+    };
     let mut network = Network::create_from_config(&network_config).await;
     let compute_nodes = &network_config.nodes[&NodeType::Compute];
     let storage_nodes = &network_config.nodes[&NodeType::Storage];
     let miner_nodes = &network_config.nodes[&NodeType::Miner];
-
-    let mut tx_generator = TransactionGen::new(
-        (0..seed_wallet_count)
-            .map(|i| wallet_seed((i, "000000"), &initial_amount))
-            .collect(),
-    );
-    let setup = UserNodeSetup {
-        user_setup_tx_chunk_size: Some(5),
-        user_setup_tx_in_per_tx: Some(3),
-        user_setup_tx_max_count: tx_max_count,
-        ..Default::default()
-    };
 
     //
     // Act
@@ -1302,11 +1298,7 @@ async fn main_loops_raft_1_node_common(
 
                 while let Some(response) = node.handle_next_event(&mut test_timeout()).await {
                     panic_on_timeout(&response, &node_name);
-                    if node
-                        .handle_next_event_response(&setup, &mut tx_generator, response)
-                        .await
-                        == ResponseResult::Exit
-                    {
+                    if node.handle_next_event_response(response).await == ResponseResult::Exit {
                         break;
                     }
                 }
@@ -2933,6 +2925,7 @@ fn complete_network_config(initial_port: u16) -> NetworkConfig {
             .collect(),
         test_duration_divider: TEST_DURATION_DIVIDER,
         passphrase: Some("Test Passphrase".to_owned()),
+        test_auto_gen_setup: Default::default(),
     }
 }
 

src/upgrade/tests.rs
@@ -373,6 +373,7 @@ fn complete_network_config(initial_port: u16) -> NetworkConfig {
             .collect(),
         test_duration_divider: 1,
         passphrase: Some(WALLET_PASSWORD.to_owned()),
+        test_auto_gen_setup: Default::default(),
     }
 }
 

src/user.rs
@@ -1,5 +1,5 @@
 use crate::comms_handler::{CommsError, Event, Node};
-use crate::configurations::{ExtraNodeParams, UserNodeConfig, UserNodeSetup};
+use crate::configurations::{ExtraNodeParams, UserAutoGenTxSetup, UserNodeConfig};
 use crate::constants::PEER_LIMIT;
 use crate::interfaces::{ComputeRequest, NodeType, Response, UseInterface, UserRequest};
 use crate::transaction_gen::TransactionGen;
@@ -88,6 +88,15 @@ pub struct ReturnPayment {
 
 /// An instance of a MinerNode
 #[derive(Debug)]
+pub struct AutoGenTx {
+    tx_generator: TransactionGen,
+    tx_chunk_size: Option<usize>,
+    tx_in_per_tx: Option<usize>,
+    tx_max_count: usize,
+}
+
+/// An instance of a MinerNode
+#[derive(Debug)]
 pub struct UserNode {
     node: Node,
     wallet_db: WalletDb,
@@ -97,6 +106,7 @@ pub struct UserNode {
     trading_peer: Option<(SocketAddr, TokenAmount)>,
     next_payment: Option<(SocketAddr, Transaction)>,
     last_block_notified: Block,
+    test_auto_gen_tx: Option<AutoGenTx>,
 }
 
 impl UserNode {
@@ -128,6 +138,9 @@ impl UserNode {
         .with_seed(config.user_node_idx, &config.user_wallet_seeds)
         .await;
 
+        let test_auto_gen_tx =
+            make_transaction_gen(config.test_auto_gen_setup, config.user_node_idx);
+
         Ok(UserNode {
             node,
             wallet_db,
@@ -137,6 +150,7 @@ impl UserNode {
             trading_peer: None,
             next_payment: None,
             last_block_notified: Default::default(),
+            test_auto_gen_tx,
         })
     }
 
@@ -148,6 +162,11 @@ impl UserNode {
     /// Returns the node's compute endpoint.
     pub fn compute_address(&self) -> SocketAddr {
         self.compute_addr
+    }
+
+    /// Check if auto generator is active
+    pub fn is_test_auto_gen_tx_active(&self) -> bool {
+        self.test_auto_gen_tx.is_some()
     }
 
     /// Connect to a peer on the network.
@@ -189,8 +208,6 @@ impl UserNode {
     /// Listens for new events from peers and handles them, processing any errors.
     pub async fn handle_next_event_response(
         &mut self,
-        setup: &UserNodeSetup,
-        tx_generator: &mut TransactionGen,
         response: Result<Response>,
     ) -> ResponseResult {
         debug!("Response: {:?}", response);
@@ -222,53 +239,7 @@ impl UserNode {
                 success: true,
                 reason: "Block mining notified",
             }) => {
-                // Load any data after restart
-                if !tx_generator.is_up_to_date_with_snapshot() {
-                    if let Some(v) = self.wallet_db.get_db_value(TX_GENERATOR_KEY).await {
-                        tx_generator.apply_snapshot_state(&v);
-                    }
-                }
-
-                // Process committed transactions
-                {
-                    let txs = &self.last_block_notified.transactions;
-                    info!("Block notified with txs: {}", txs.len());
-                    tx_generator.commit_transactions(txs);
-                }
-
-                // Send next transactions
-                let mut total_txs = 0;
-                loop {
-                    let txs_chunk_len = std::cmp::min(
-                        setup.user_setup_tx_chunk_size.unwrap_or(usize::MAX),
-                        setup.user_setup_tx_max_count.saturating_sub(total_txs),
-                    );
-                    let txs_chunk: Vec<_> = tx_generator
-                        .make_all_transactions(setup.user_setup_tx_in_per_tx, txs_chunk_len)
-                        .into_iter()
-                        .map(|(_, tx)| tx)
-                        .collect();
-
-                    if txs_chunk.is_empty() {
-                        break;
-                    }
-
-                    total_txs += txs_chunk.len();
-                    debug!("New Generated txs:{:?}", txs_chunk);
-                    if let Err(e) = self
-                        .send_transactions_to_compute(self.compute_address(), txs_chunk)
-                        .await
-                    {
-                        error!("Autogenerated tx not sent to {:?}", e);
-                    }
-                }
-                if total_txs > 0 {
-                    info!("New Generated txs sent: {}", total_txs);
-                }
-
-                self.wallet_db
-                    .set_db_value(TX_GENERATOR_KEY, tx_generator.snapshot_state())
-                    .await;
+                self.process_mining_notified().await;
             }
             Ok(Response {
                 success: true,
@@ -631,6 +602,66 @@ impl UserNode {
         }
     }
 
+    /// Process a notification for block mining to auto generate next transactions
+    pub async fn process_mining_notified(&mut self) {
+        if self.test_auto_gen_tx.is_none() {
+            return;
+        }
+
+        let auto_gen = self.test_auto_gen_tx.as_mut().unwrap();
+
+        // Load any data after restart
+        if !auto_gen.tx_generator.is_up_to_date_with_snapshot() {
+            if let Some(v) = self.wallet_db.get_db_value(TX_GENERATOR_KEY).await {
+                auto_gen.tx_generator.apply_snapshot_state(&v);
+            }
+        }
+
+        // Process committed transactions
+        {
+            let txs = &self.last_block_notified.transactions;
+            info!("Block notified with txs: {}", txs.len());
+            auto_gen.tx_generator.commit_transactions(txs);
+        }
+
+        // Send next transactions
+        let mut total_txs = 0;
+        loop {
+            let auto_gen = self.test_auto_gen_tx.as_mut().unwrap();
+            let txs_chunk_len = std::cmp::min(
+                auto_gen.tx_chunk_size.unwrap_or(usize::MAX),
+                auto_gen.tx_max_count.saturating_sub(total_txs),
+            );
+            let txs_chunk: Vec<_> = auto_gen
+                .tx_generator
+                .make_all_transactions(auto_gen.tx_in_per_tx, txs_chunk_len)
+                .into_iter()
+                .map(|(_, tx)| tx)
+                .collect();
+
+            if txs_chunk.is_empty() {
+                break;
+            }
+
+            total_txs += txs_chunk.len();
+            debug!("New Generated txs:{:?}", txs_chunk);
+            if let Err(e) = self
+                .send_transactions_to_compute(self.compute_address(), txs_chunk)
+                .await
+            {
+                error!("Autogenerated tx not sent to {:?}", e);
+            }
+        }
+        if total_txs > 0 {
+            info!("New Generated txs sent: {}", total_txs);
+        }
+
+        let auto_gen = self.test_auto_gen_tx.as_mut().unwrap();
+        self.wallet_db
+            .set_db_value(TX_GENERATOR_KEY, auto_gen.tx_generator.snapshot_state())
+            .await;
+    }
+
     // Get the wallet db
     pub fn get_wallet_db(&self) -> &WalletDb {
         &self.wallet_db
@@ -654,5 +685,23 @@ impl UseInterface for UserNode {
             success: true,
             reason: "New address ready to be sent",
         }
+    }
+}
+
+fn make_transaction_gen(setup: UserAutoGenTxSetup, user_node_idx: usize) -> Option<AutoGenTx> {
+    let initial_transactions = setup
+        .user_initial_transactions
+        .get(user_node_idx)
+        .cloned()
+        .unwrap_or_default();
+    if !initial_transactions.is_empty() && setup.user_setup_tx_max_count != 0 {
+        Some(AutoGenTx {
+            tx_generator: TransactionGen::new(initial_transactions),
+            tx_chunk_size: setup.user_setup_tx_chunk_size,
+            tx_in_per_tx: setup.user_setup_tx_in_per_tx,
+            tx_max_count: setup.user_setup_tx_max_count,
+        })
+    } else {
+        None
     }
 }

src/test_utils.rs
@@ -6,7 +6,7 @@ use crate::comms_handler::Node;
 use crate::compute::ComputeNode;
 use crate::configurations::{
     ComputeNodeConfig, DbMode, ExtraNodeParams, MinerNodeConfig, NodeSpec, StorageNodeConfig,
-    UserNodeConfig, UtxoSetSpec, WalletTxSpec,
+    UserAutoGenTxSetup, UserNodeConfig, UtxoSetSpec, WalletTxSpec,
 };
 use crate::constants::{DB_PATH, DB_PATH_TEST, WALLET_PATH};
 use crate::miner::MinerNode;
@@ -69,6 +69,7 @@ pub struct NetworkConfig {
     pub compute_to_miner_mapping: BTreeMap<String, Vec<String>>,
     pub test_duration_divider: usize,
     pub passphrase: Option<String>,
+    pub test_auto_gen_setup: UserAutoGenTxSetup,
 }
 
 impl NetworkConfig {
@@ -855,6 +856,7 @@ async fn init_user(
         api_port: 3000,
         user_wallet_seeds: config.user_wallet_seeds.clone(),
         passphrase: config.passphrase.clone(),
+        test_auto_gen_setup: config.test_auto_gen_setup.clone(),
     };
 
     let info = format!("{} -> {}", name, node_info.node_spec.address);

src/bin/user.rs
@@ -3,12 +3,12 @@
 use clap::{App, Arg};
 use naom::primitives::asset::TokenAmount;
 use std::net::SocketAddr;
-use system::configurations::{UserNodeConfig, UserNodeSetup};
+use system::configurations::UserNodeConfig;
 use system::{
     loop_wait_connnect_to_peers_async, loops_re_connect_disconnect, shutdown_connections,
     ResponseResult,
 };
-use system::{routes, TransactionGen, UserNode};
+use system::{routes, UserNode};
 
 #[tokio::main]
 async fn main() {
@@ -69,7 +69,7 @@ async fn main() {
         )
         .get_matches();
 
-    let (setup, config) = {
+    let config = {
         let mut settings = config::Config::default();
         let setting_file = matches
             .value_of("config")
@@ -117,12 +117,10 @@ async fn main() {
             settings.set("passphrase", index).unwrap();
         }
 
-        let setup: UserNodeSetup = settings.clone().try_into().unwrap();
         let config: UserNodeConfig = settings.try_into().unwrap();
-        (setup, config)
+        config
     };
     println!("Starting node with config: {:?}", config);
-    println!("Start node with setup {:?}", setup);
     println!();
 
     // Handle a payment amount
@@ -132,7 +130,6 @@ async fn main() {
         Some(Err(e)) => panic!("Unable to pay with amount specified due to error: {:?}", e),
     };
 
-    let user_node_idx = config.user_node_idx;
     let peer_user_node = *config.user_nodes.get(config.peer_user_node_idx).unwrap();
     let mut node = UserNode::new(config, Default::default()).await.unwrap();
 
@@ -166,21 +163,8 @@ async fn main() {
             .unwrap();
     }
 
-    // Generate automatic transactions
-    let (tx_generator_active, tx_generator) = {
-        let initial_transactions = setup
-            .user_initial_transactions
-            .get(user_node_idx)
-            .cloned()
-            .unwrap_or_default();
-        (
-            !initial_transactions.is_empty() && setup.user_setup_tx_max_count != 0,
-            TransactionGen::new(initial_transactions),
-        )
-    };
-
     // send notification request
-    if tx_generator_active {
+    if node.is_test_auto_gen_tx_active() {
         node.send_block_notification_request(node.compute_address())
             .await
             .unwrap();
@@ -189,17 +173,12 @@ async fn main() {
     // REQUEST HANDLING
     let main_loop_handle = tokio::spawn({
         let mut node = node;
-        let mut tx_generator = tx_generator;
         let mut node_conn = node_conn;
 
         async move {
             let mut exit = std::future::pending();
             while let Some(response) = node.handle_next_event(&mut exit).await {
-                if node
-                    .handle_next_event_response(&setup, &mut tx_generator, response)
-                    .await
-                    == ResponseResult::Exit
-                {
+                if node.handle_next_event_response(response).await == ResponseResult::Exit {
                     break;
                 }
             }

commit 37a1c8a10b8b3035ca7225b195b799abe99f0c46

Fix broken user binary loading auto generated tx
This commit 793189a58b5396dcd1010be6e5adeabc8697ddf7 broke the configuration
loading by adding hierarchy.

-Update configuration files to follow the same hierarchy.
-Use same naming convention prefix

Test:
cargo clippy
src/bin/node_settings_local_raft_2_run.sh
=> user_0.log no panic.

src/bin/node_settings.toml
@@ -8,6 +8,8 @@ storage_raft = 0
 compute_partition_full_size = 1
 compute_minimum_miner_pool_len = 1
 jurisdiction = "US"
+
+[user_test_auto_gen_setup]
 user_setup_tx_chunk_size = 5
 user_setup_tx_in_per_tx = 3
 user_setup_tx_max_count = 100000

src/bin/node_settings_local_raft_1.toml
@@ -8,6 +8,8 @@ storage_raft = 1
 compute_partition_full_size = 1
 compute_minimum_miner_pool_len = 1
 jurisdiction = "US"
+
+[user_test_auto_gen_setup]
 user_setup_tx_chunk_size = 5
 user_setup_tx_in_per_tx = 3
 user_setup_tx_max_count = 100000

src/bin/node_settings_local_raft_2.toml
@@ -6,6 +6,8 @@ compute_raft = 1
 storage_raft = 1
 compute_partition_full_size = 2
 compute_minimum_miner_pool_len = 3
+
+[user_test_auto_gen_setup]
 user_setup_tx_chunk_size = 5
 user_setup_tx_in_per_tx = 3
 user_setup_tx_max_count = 100000

src/bin/node_settings_local_raft_3.toml
@@ -6,6 +6,8 @@ compute_raft = 1
 storage_raft = 1
 compute_partition_full_size = 2
 compute_minimum_miner_pool_len = 3
+
+[user_test_auto_gen_setup]
 user_setup_tx_chunk_size = 5
 user_setup_tx_in_per_tx = 3
 user_setup_tx_max_count = 100000

src/configurations.rs
@@ -146,7 +146,7 @@ pub struct UserNodeConfig {
     /// Option of the passphrase used for encryption
     pub passphrase: Option<String>,
     /// Configuration options for auto generating transactions for test
-    pub test_auto_gen_setup: UserAutoGenTxSetup,
+    pub user_test_auto_gen_setup: UserAutoGenTxSetup,
 }
 
 /// Configuration option for setup of user node

src/tests.rs
@@ -1202,7 +1202,7 @@ async fn main_loops_raft_1_node_common(
     //
     network_config.compute_seed_utxo =
         make_compute_seed_utxo(&[(seed_count, "000000")], initial_amount);
-    network_config.test_auto_gen_setup = UserAutoGenTxSetup {
+    network_config.user_test_auto_gen_setup = UserAutoGenTxSetup {
         user_initial_transactions: vec![(0..seed_wallet_count)
             .map(|i| wallet_seed((i, "000000"), &initial_amount))
             .collect()],
@@ -2839,7 +2839,7 @@ fn complete_network_config(initial_port: u16) -> NetworkConfig {
             .collect(),
         test_duration_divider: TEST_DURATION_DIVIDER,
         passphrase: Some("Test Passphrase".to_owned()),
-        test_auto_gen_setup: Default::default(),
+        user_test_auto_gen_setup: Default::default(),
     }
 }
 

src/upgrade/tests.rs
@@ -458,7 +458,7 @@ fn complete_network_config(initial_port: u16) -> NetworkConfig {
             .collect(),
         test_duration_divider: 1,
         passphrase: Some(WALLET_PASSWORD.to_owned()),
-        test_auto_gen_setup: Default::default(),
+        user_test_auto_gen_setup: Default::default(),
     }
 }
 

src/user.rs
@@ -139,7 +139,7 @@ impl UserNode {
         .await;
 
         let test_auto_gen_tx =
-            make_transaction_gen(config.test_auto_gen_setup, config.user_node_idx);
+            make_transaction_gen(config.user_test_auto_gen_setup, config.user_node_idx);
 
         Ok(UserNode {
             node,

src/test_utils.rs
@@ -72,7 +72,7 @@ pub struct NetworkConfig {
     pub compute_to_miner_mapping: BTreeMap<String, Vec<String>>,
     pub test_duration_divider: usize,
     pub passphrase: Option<String>,
-    pub test_auto_gen_setup: UserAutoGenTxSetup,
+    pub user_test_auto_gen_setup: UserAutoGenTxSetup,
 }
 
 impl NetworkConfig {
@@ -965,7 +965,7 @@ async fn init_user(
         api_port: 3000,
         user_wallet_seeds: config.user_wallet_seeds.clone(),
         passphrase: config.passphrase.clone(),
-        test_auto_gen_setup: config.test_auto_gen_setup.clone(),
+        user_test_auto_gen_setup: config.user_test_auto_gen_setup.clone(),
     };
 
     let info = format!("{} -> {}", name, node_info.node_spec.address);