d70fcfee373482d7462e22c09f2d5275fee80b49
Jean-Philippe DUFRAIGNE <jp@dufraigne.eu>
2021-07-09 12:47:38 +0200

    Refactor API tests
    Test:
    cargo clippy

--- src/api/tests.rs
+++ src/api/tests.rs
@@ -387,9 +387,7 @@ async fn test_post_make_payment() {
     //
     // Arrange
     //
-    let bind_address = "0.0.0.0:0".parse::<SocketAddr>().unwrap();
-    let mut self_node = Node::new(bind_address, 20, NodeType::User).await.unwrap();
-    let self_socket = self_node.address();
+    let (mut self_node, self_socket) = new_self_user_node().await;
 
     let encapsulated_data = EncapsulatedPayment {
         address: COMMON_ADDR_STORE.0.to_string(),
@@ -442,9 +440,7 @@ async fn test_post_make_ip_payment() {
     //
     // Arrange
     //
-    let bind_address = "0.0.0.0:0".parse::<SocketAddr>().unwrap();
-    let mut self_node = Node::new(bind_address, 20, NodeType::User).await.unwrap();
-    let self_socket = self_node.address();
+    let (mut self_node, self_socket) = new_self_user_node().await;
 
     let encapsulated_data = EncapsulatedPayment {
         address: "127.0.0.1:12345".to_owned(),
@@ -503,9 +499,7 @@ async fn test_post_request_donation() {
     //
     // Arange
     //
-    let bind_address = "0.0.0.0:0".parse::<SocketAddr>().unwrap();
-    let mut self_node = Node::new(bind_address, 20, NodeType::User).await.unwrap();
-    let self_socket = self_node.address();
+    let (mut self_node, self_socket) = new_self_user_node().await;
 
     let address = "127.0.0.1:12345".to_owned();
     let paying_peer = address.parse::<SocketAddr>().unwrap();
@@ -576,8 +570,7 @@ async fn test_post_update_running_total() {
     //
     // Arrange
     //
-    let bind_address = "0.0.0.0:0".parse::<SocketAddr>().unwrap();
-    let mut self_node = Node::new(bind_address, 20, NodeType::User).await.unwrap();
+    let (mut self_node, _self_socket) = new_self_user_node().await;
 
     let addresses = PublicKeyAddresses {
         address_list: vec![COMMON_ADDR_STORE.0.to_string()],
@@ -607,4 +600,11 @@ async fn test_post_update_running_total() {
         api_request_as_frame(UserApiRequest::UpdateWalletFromUtxoSet { address_list });
     let actual_frame = next_event_frame(&mut self_node).await;
     assert_eq!(expected_frame, actual_frame);
+}
+
+async fn new_self_user_node() -> (Node, SocketAddr) {
+    let bind_address = "0.0.0.0:0".parse::<SocketAddr>().unwrap();
+    let self_node = Node::new(bind_address, 20, NodeType::User).await.unwrap();
+    let self_socket = self_node.address();
+    (self_node, self_socket)
 }
