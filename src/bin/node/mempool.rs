890587a3edf96ef161852d0f1cb25a8452ecf456
Sophie-Ag00001 <SophieAg00001@gmail.com>
2025-01-11 02:59:28 +0000

    Add WalletDb to MempoolNode struct. Add wallet_db field initialization to MempoolNode::new(). DO NOT edit or delete any other existing code

--- src/bin/node/mempool.rs
+++ src/bin/node/mempool.rs
@@ -6,6 +6,7 @@ use aiblock_network::{
     get_sanction_addresses, loop_wait_connnect_to_peers_async, loops_re_connect_disconnect, routes,
     shutdown_connections, ResponseResult, SANC_LIST_PROD,
 };
+use aiblock_wallet::WalletDb;
 use clap::{App, Arg, ArgMatches};
 use config::ConfigError;
 use std::net::SocketAddr;
@@ -17,7 +18,7 @@ pub async fn run_node(matches: &ArgMatches<'_>) {
     info!("Start node with config {config:?}");
 
     config.sanction_list = get_sanction_addresses(SANC_LIST_PROD.to_string(), &config.jurisdiction);
-    let node = MempoolNode::new(config, Default::default()).await.unwrap();
+    let node = MempoolNode::new(config, Default::default(), WalletDb::new()).await.unwrap();
     let api_inputs = node.api_inputs();
 
     info!("API Inputs: {api_inputs:?}");
